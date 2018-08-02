d05c45f92b4b4e54bc8ea3865b2d4206dc802906
Amalie S <amaliesmidth@gmail.com>
2018-08-02 16:06:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+function getFirstSelector(selector) {
+  const firstElement = document.querySelector(selector)
+  return firstElement
+}
+
+// function getFirstSelector(selector) {
+//   return document.querySelector(selector)
+// }
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+// note on above  ---- when using querySelectorAll & querySelector you can only ever pass ONE argument. anything more than this will be ignored.    in above example, nested and target are part of the same string 
+
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node') 
+  let nextNode = grandNode.children[0]
+  
+  while (nextNode) {
+    grandNode = nextNode
+    nextNode = grandNode.children[0]
+  }
+  return grandNode
+}
+
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstRankedList = rankedLists[0]
+  const secondRankedList = rankedLists[1]
+  
+  let firstRankedListChildren = firstRankedList.children
+  for (let i = 0; i < firstRankedListChildren.length; i++) {
+    let value = parseInt(firstRankedListChildren[i].innerHTML)
+    return value + n
+  }
+  
+}
+
+
+
 
